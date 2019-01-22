9bc98ef6ffe056e7a3b5010ca7c2f84096061bc8
Unknown <tofferlinville@gmail.com>
2019-01-22 10:29:13 -0700

    Added Windows

---
+++ Adventure/game.js
@@ -0,0 +1,16 @@
+// Single Line Comment
+/*
+
+Multi-Line Comment
+
+*/
+
+/*
+alert("Warning!! Will Robinson... Warning!!");
+
+confirm("Do you like Pokemon");
+
+prompt("What type of Pokemon do you like?");*/
+var pc = promt("What is your name?");
+
+document.write(pc);
