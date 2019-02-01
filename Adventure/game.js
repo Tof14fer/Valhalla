185f61affee1bb989d3fd3dae859d10e9ceb1134
Unknown <tofferlinville@gmail.com>
2019-02-01 10:25:10 -0700

    Added Two New Paths

--- Adventure/game.js
+++ Adventure/game.js
@@ -33,7 +33,30 @@ alert("The Legend of the Really Dark Forest");
             
             var forestLook = confirm("About three feet in front of you there is a super conveniently placed flash light, you're welcome. \n -Would you like to take the take the Flashlight?");
             if (forestLook){
-                alert ("A Flashlight was added to your inventory.");}
+                alert ("A Flashlight was added to your inventory.");
+                
+            var directiontogo = prompt("Now that you have a light you can see some of the things that are lurking around you. You notice there are two paths you can take. You can either go north or west. Which way do you want to go? \n -North \n -West").toLowerCase();{
+                
+                if(directiontogo == "north"){
+                
+            var northboundtown = alert("So, you see a small town further along the path.. Would you like to continue to the town or turn back?... Hah like you had a choice.");
+                alert ("Ok, then lets get to that town!!");
+                Town();}
+                
+            else if(directiontogo == "west"){
+                
+                var westward = alert ("Well.. I guess we are heading further into the forest. Here we go!!!");
+                    DeepForest();
+                }
+            }
+                
+                
+                
+                
+                
+                
+                
+            }
             
             else if (!forestLook){
                 alert ("Something really big and mean likes the dark as cover. This unfaced beast comes up behind you and \nBAM!! \nYou Died..");}
@@ -47,15 +70,26 @@ alert("The Legend of the Really Dark Forest");
             Forest();
         }
         else if (!cryingabit){
-            alert ("Well then I guest this is goodbye... Game Over.")
+            alert ("Well then I guest this is goodbye... Game Over.");
         }
     }
-        else if(forest1 == "run and scream" || forest1 == "run" || forest1 == "scream");
-    alert("You hit a tree and ")    
+        else if(forest1 == "run and scream" || forest1 == "run" || forest1 == "scream"){
+    alert("You start at a dead sprint... sadly you are in the dark so you hit a tree and passed out!! You wake up right where you started.");
+        }
+            Forest();
+        
    //Forests end bracket
     }
+    function Town(){
+        var township = alert("Welcome to our Town.");
+    }
+    Town();
     
     
+    function DeepForest(){
+        var forest2 = prompt("OOF this is spooky..");
+    }
+    DeepForest();
     
  
 }
